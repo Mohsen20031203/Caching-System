997df0fcf439c3790ae11cc69221ec7345387f51
mohsen <168640039+Mohsen20031203@users.noreply.github.com>
2025-03-15 14:46:16 +0330

    edit api get chat

--- massager/api/server.go
+++ massager/api/server.go
@@ -49,6 +49,7 @@ func (s *Server) setupRouter() {
 	Users.PUT("/read/:id", s.Read)
 
 	Massage := router.Group("/massage")
+	Massage.GET("/chat/:sender_id/:receiver_id")
 	Massage.Use(s.GetCache)
 
 	s.Router = router
