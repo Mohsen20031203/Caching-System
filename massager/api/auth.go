311b2eb4019a7ca40e896cc3e5dd36a847cb0b9d
mohsen <168640039+Mohsen20031203@users.noreply.github.com>
2025-04-05 10:08:32 +0330

    create auth.go

---
+++ massager/api/auth.go
@@ -0,0 +1,26 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (server *Server) generateToken(ctx *gin.Context, username string, id int64, phone string) {
+	accessToken, err := server.Jwt.AccessToken(username, id, phone)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "internal server error"})
+		return
+	}
+
+	refreshToken, err := server.Jwt.RefreshToken(username, id)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "internal server error"})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"access_token":  accessToken,
+		"refresh_token": refreshToken,
+	})
+}
