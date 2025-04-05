d8f4932c3ba94c7aee0ea812a4074a55275f65ba
mohsen <168640039+Mohsen20031203@users.noreply.github.com>
2025-04-05 10:50:16 +0330

    delete CreatUser in api

--- massager/api/user.go
+++ massager/api/user.go
@@ -8,26 +8,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func (s *Server) CreatUser(ctx *gin.Context) {
-	var user models.User
-
-	err := ctx.Bind(&user)
-	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{
-			"error": "bad request",
-		})
-		return
-	}
-	err = s.Store.CreatUser(&user)
-	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{
-			"error": "internal server error",
-		})
-	}
-	ctx.JSON(http.StatusCreated, user)
-
-}
-
 func (s *Server) GetUser(ctx *gin.Context) {
 	id := ctx.Param("id")
 
