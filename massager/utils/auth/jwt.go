bcfe2eb1c8f24729db875963c3857aecb5ccd176
mohsen <168640039+Mohsen20031203@users.noreply.github.com>
2025-04-05 15:24:35 +0330

    check token

--- massager/utils/auth/jwt.go
+++ massager/utils/auth/jwt.go
@@ -2,6 +2,7 @@ package auth
 
 import (
 	"chach/massager/config"
+	"fmt"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -36,7 +37,7 @@ func (j *JWTtoken) AccessToken(username string, id int64, phone string) (string,
 		string(ClmPhone):    phone,
 		string(ClmUsername): username,
 		//"exp":               time.Now().Add(Minute * 5).Unix(),
-		"exp": time.Now().Add(time.Hour * 24).Unix(),
+		"exp": time.Now().Add(time.Minute * 5).Unix(),
 	}
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	return token.SignedString(j.JWT_SECRET_KEY)
@@ -48,22 +49,54 @@ func (j *JWTtoken) RefreshToken(username string, id int64) (string, error) {
 		string(ClmId):       id,
 		string(ClmUsername): username,
 		// "exp":               time.Now().Add(time.Hour).Unix(),
-		"exp": time.Now().Add(time.Hour * 240).Unix(),
+		"exp": time.Now().Add(time.Minute * 10).Unix(),
 	}
 	refreshToken := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	return refreshToken.SignedString(j.JWT_REFRESH_SECRET_KEY)
 }
 
 func (j *JWTtoken) CheckToken(ctx *gin.Context) {
+	token, err := j.parseToken(ctx)
+	if err != nil || !token.Valid {
+		ctx.JSON(401, gin.H{"error": "Invalid token"})
+		ctx.Abort()
+		return
+	}
+	fmt.Println(ctx.Request.URL.Path)
+
 	authHeader := ctx.GetHeader("Authorization")
 	if authHeader == "" {
 		ctx.JSON(401, gin.H{"error": "Authorization header missing"})
 		ctx.Abort()
 	}
 
-	if authHeader != string(j.JWT_SECRET_KEY) {
-		ctx.JSON(401, gin.H{"error": "Authorization header missing"})
-
-	}
 	ctx.Next()
+}
+
+func (j *JWTtoken) parseToken(ctx *gin.Context) (*jwt.Token, error) {
+	authHeader := ctx.GetHeader("Authorization")
+	if authHeader == "" {
+		ctx.JSON(401, gin.H{"error": "Authorization header missing"})
+		ctx.Abort()
+		return nil, nil
+	}
+
+	const bearerPrefix = "Bearer "
+	tokenF := fmt.Sprintf("%s%s", bearerPrefix, authHeader)
+	if len(tokenF) <= len(bearerPrefix) || tokenF[:len(bearerPrefix)] != bearerPrefix {
+		ctx.JSON(401, gin.H{"error": "Invalid Authorization header format"})
+		ctx.Abort()
+		return nil, nil
+	}
+	return parseToken(authHeader, string(j.JWT_SECRET_KEY))
+}
+
+func parseToken(tokenString string, secretKey string) (*jwt.Token, error) {
+	m, n := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(secretKey), nil
+	})
+	return m, n
 }
