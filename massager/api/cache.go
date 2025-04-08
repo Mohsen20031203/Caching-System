bf8b202a6cd5ee485f524c84db3d7c71fd50146e
mohsen <168640039+Mohsen20031203@users.noreply.github.com>
2025-04-08 13:35:59 +0330

    rename and add middlewares in api

--- massager/api/cache.go
+++ massager/api/cache.go
@@ -17,7 +17,7 @@ type CustomResponseWriter struct {
 	Body *bytes.Buffer
 }
 
-func (s *Server) AutoCache(ctx *gin.Context) {
+func (s *Server) LongCache(ctx *gin.Context) {
 
 	key := fmt.Sprintf("%s|%s|%s",
 		ctx.Request.Host,
@@ -55,7 +55,96 @@ func (s *Server) AutoCache(ctx *gin.Context) {
 	}
 
 	if ctx.Writer.Status() == http.StatusOK {
-		err = s.RDB.Set(ctx, key, jsonData, 10*time.Minute).Err()
+		err = s.RDB.Set(ctx, key, jsonData, 5*time.Minute).Err()
+		if err != nil {
+			return
+		}
+	}
+}
+
+func (s *Server) ShortCache(ctx *gin.Context) {
+
+	key := fmt.Sprintf("%s|%s|%s",
+		ctx.Request.Host,
+		ctx.Request.RequestURI,
+		ctx.Request.URL.RawQuery,
+	)
+	val, err := s.RDB.Get(ctx, key).Result()
+	if err == nil && ctx.Request.Method == "GET" {
+		ctx.Abort()
+
+		var rawData json.RawMessage
+		if err := json.Unmarshal([]byte(val), &rawData); err == nil {
+			ctx.Data(http.StatusOK, "application/json", rawData)
+		} else {
+			ctx.JSON(http.StatusOK, gin.H{"data": val})
+		}
+
+		return
+	} else if err != redis.Nil && ctx.Request.Method == "GET" {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "cache retrieval error"})
+		return
+	}
+
+	ctx.Next()
+	keyContext, status := ctx.Get(ctx.Request.RequestURI)
+	if !status {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "cache retrieval error"})
+		return
+	}
+
+	jsonData, err := json.Marshal(keyContext)
+	if err != nil {
+		log.Println("Error marshaling data:", err)
+		return
+	}
+
+	if ctx.Writer.Status() == http.StatusOK {
+		err = s.RDB.Set(ctx, key, jsonData, 2*time.Second).Err()
+		if err != nil {
+			return
+		}
+	}
+}
+func (s *Server) MiddleCache(ctx *gin.Context) {
+
+	key := fmt.Sprintf("%s|%s|%s",
+		ctx.Request.Host,
+		ctx.Request.RequestURI,
+		ctx.Request.URL.RawQuery,
+	)
+	val, err := s.RDB.Get(ctx, key).Result()
+	if err == nil && ctx.Request.Method == "GET" {
+		ctx.Abort()
+
+		var rawData json.RawMessage
+		if err := json.Unmarshal([]byte(val), &rawData); err == nil {
+			ctx.Data(http.StatusOK, "application/json", rawData)
+		} else {
+			ctx.JSON(http.StatusOK, gin.H{"data": val})
+		}
+
+		return
+	} else if err != redis.Nil && ctx.Request.Method == "GET" {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "cache retrieval error"})
+		return
+	}
+
+	ctx.Next()
+	keyContext, status := ctx.Get(ctx.Request.RequestURI)
+	if !status {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "cache retrieval error"})
+		return
+	}
+
+	jsonData, err := json.Marshal(keyContext)
+	if err != nil {
+		log.Println("Error marshaling data:", err)
+		return
+	}
+
+	if ctx.Writer.Status() == http.StatusOK {
+		err = s.RDB.Set(ctx, key, jsonData, 2*time.Minute).Err()
 		if err != nil {
 			return
 		}
